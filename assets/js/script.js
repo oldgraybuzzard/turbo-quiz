95d6cf9ebd8c89db89938f733e6516b634c8cfaf
oldgraybuzzard <k_felder@me.com>
2021-09-14 17:49:59 -0400

    code setup

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var testerName = prompt("What is your first name?");
+var testerInitials = "";
+var highScore = "";
+var score = "";
+var lowScore = "";
+
+alert("Are you ready to test your coding skills " + testerName + "?");
+alert("When you click Ok, the game will start. However, before we click ok, you need toknow that you will only have 60 secs to answer 10 questions. If you choose the wrong answer, yo will loose 5 seconds. If you understand the instructions click OK, if not click Cancel to exit the game.----- NOW, ARE YOU READY?")
+
+// play ganme
+
+
+// timer
+
+// display high scores
+
+
+// correct or wrong
+
+
+// array of questions
+
+
+//
+
+
